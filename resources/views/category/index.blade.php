6694380518d6a1024acb4f6be92c581be494758e
jutt786-tech <qasimjutt840@gmail.com>
2019-09-30 10:27:20 +0500

    category table complete

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -12,7 +12,7 @@
                 <!-- Breadcrumbs-->
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">
-                        <a href="#">category</a>
+                        <a href="#">category Table</a>
                     </li>
                     <li class="breadcrumb-item active">Overview</li>
                 </ol>
